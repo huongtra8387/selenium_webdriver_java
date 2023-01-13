106d50063fb8c6dcc459e91166de738ef633bc70
Tra Phan <trapth.87.dn@gmail.com>
2023-01-13 23:01:31 +0700

    Init Repo

---
+++ selenium/javaTester/Topic_01_System_Enviroment.java
@@ -0,0 +1,11 @@
+package javaTester;
+
+public class Topic_01_System_Enviroment {
+
+	public static void main(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+
+	}
+
+}
